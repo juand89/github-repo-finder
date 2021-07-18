c2b4cd4a25900c60c87fecf479a0e51777de019e
JD <juandu89@gmail.com>
2021-07-18 19:45:05 +0900

    throttle the api search requests

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 const inputSearch = document.getElementById('search')
 const sort = document.getElementById('sort')
+var timeout = null
 var sortValue = ''
 const page = 1
 const searchQuery = `https://api.github.com/search/repositories?per_page=10&q=`
-inputSearch.addEventListener('input', async (e) => {
+inputSearch.addEventListener('input', (e) => {
   const searchValue = e.target.value
   try {
-    if (searchValue) {
-      const repositories = await fetch(
-        `${searchQuery}${searchValue}&sort=${sortValue}&page=${page}`
-      ).then((res) => res.json())
-
-      console.log(repositories)
+    if (searchValue && timeout === null) {
+      // throttle api requests for 1500 ms
+      timeout = setTimeout(async () => {
+        const repositories = await fetch(
+          `${searchQuery}${inputSearch.value}&sort=${sortValue}&page=${page}`
+        ).then((res) => res.json())
+        console.log(repositories)
+        timeout = null
+      }, 1500)
     }
   } catch (e) {
     console.error(e)
