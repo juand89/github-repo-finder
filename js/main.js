9e1507d30cdf264a75428747f7b75223900a5fcc
JD <juandu89@gmail.com>
2021-07-18 19:21:35 +0900

    add the search query api

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 const inputSearch = document.getElementById('search')
 const sort = document.getElementById('sort')
 var sortValue = ''
-inputSearch.addEventListener('input', (e) => {
+const page = 1
+const searchQuery = `https://api.github.com/search/repositories?per_page=10&q=`
+inputSearch.addEventListener('input', async (e) => {
   const searchValue = e.target.value
+  try {
+    if (searchValue) {
+      const repositories = await fetch(
+        `${searchQuery}${searchValue}&sort=${sortValue}&page=${page}`
+      ).then((res) => res.json())
+
+      console.log(repositories)
+    }
+  } catch (e) {
+    console.error(e)
+  }
 })
 sort.addEventListener('change', (e) => {
   sortValue = e.target.value
